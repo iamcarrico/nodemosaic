aaf197781d9e05ca8a4072e9c18c29e4cbc58028
Taylor Smith <tsmith@tsmithcreative.com>
2012-05-15 18:14:00 -0500

    Rewrite javascript, mode tile builder into new function, start building tiles

--- nodemosaic.js
+++ nodemosaic.js
@@ -1,4 +1,54 @@
-jQuery(document).ready(function($) {
+(function($) {
+
+function buildTile (data, tile, col, row) {
+
+	// Some messy variables we'll use later:
+	contentOpen = '<div class="_content">';
+	contentClose = '</div>';
+	linkOpen = ('<a href="#' + col + '/' + row +'">');
+	linkClose = '</a>';
+	
+	if ( typeof(data) == "string") {
+		switch (data) {
+			case "no_result":
+				$(tile)
+					.addClass('tile-empty')
+					.append(
+						contentOpen +
+						linkOpen +
+						'&bull;' +
+						linkClose + 
+						contentClose
+					);
+				break;
+				
+			case "range_error":
+				$(tile).addClass('tile-range');
+				break;
+		}
+			
+	} else if ( typeof(data) == "object" ) {
+		tileImage = (
+			'<img src="' +
+			data['image'] +
+			'" alt="' +
+			data['title'] + 
+			'" />'
+		);
+		$(tile)
+			.append(
+				contentOpen +
+				linkOpen +
+				tileImage +
+				linkClose + 
+				contentClose
+			)
+			.children('img')
+			.fadeIn();
+	}
+}
+
+jQuery(document).ready(function() {
 	
 	var tile_options = {
 		width: 100,
@@ -12,7 +62,7 @@ jQuery(document).ready(function($) {
 			$.getJSON(
 				('/nodemosaic/tile/' + col + '/' + row),
 				function(data){
-					$(tile).append(data);
+					buildTile(data, tile, col, row);
 				}
 			);
 		}
@@ -20,3 +70,5 @@ jQuery(document).ready(function($) {
 	
 	$.infinitedrag("#nodemosaic-wall", {}, tile_options);
 });
+
+})(jQuery);
