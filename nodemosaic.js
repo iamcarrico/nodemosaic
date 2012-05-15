5f6c2ae99dac55e5919dc70daa6153b24b08dbda
Taylor Smith <tsmith@tsmithcreative.com>
2012-05-14 23:17:50 -0500

    Adjust mosaic display and add field access control -- NOT WORKING ATM The hook_field_access() won't fire

--- nodemosaic.js
+++ nodemosaic.js
@@ -3,10 +3,10 @@ jQuery(document).ready(function($) {
 	var tile_options = {
 		width: 100,
 		height: 100,
-		start_col: 100,
-		start_row: 100,
-		range_col: [0, 200],
-		range_row: [0, 200],
+		start_col: 500,
+		start_row: 500,
+		range_col: [1, 1000],
+		range_row: [1, 1000],
 		
 		oncreate: function($element, col, row) {
 			$('<div class="_content"></div>').
